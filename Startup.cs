f881245f5a5133a6a6845017a7aedd5ef9737d3a
Sankar2202 <sankarramasamy22@gmail.com>
2024-05-27 23:42:02 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,31 @@
+﻿using WebApplication1.Services;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddSingleton<TopUpService>(); // Register the TopUpService as a singleton
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers(); // Ensure API routes are also mapped
+            });
+        }
+    }
+
+}
